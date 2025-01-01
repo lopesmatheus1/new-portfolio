b1604386114242f2794414b2fb90276b106fcaa7
Matheus Lopes Marendino <91556477+lopesmatheus1@users.noreply.github.com>
2025-01-01 20:16:38 -0300

    feat: ensuring correctly formatted dates

--- app/_components/experience-card.tsx
+++ app/_components/experience-card.tsx
@@ -15,26 +15,34 @@ const ExperienceCard = ({ experiences }: ExperienceCardProps) => {
           <CardContent className="pt-6">
             <div className="flex flex-col justify-between gap-10 lg:flex-row">
               {/* PARTE DA ESQUERDA */}
-              <div className="max-w-[384px]">
+              <div className="max-w-[384px] space-y-1">
                 <h2>{experience.title}</h2>
-                <span className="font-light text-primary/80">
+                <span className="font-medium text-primary/80">
                   {experience.contractType}
                 </span>
-                <div className="flex items-center justify-start gap-2 text-sm font-extralight">
-                  <p>Junho 2023</p>
+                <div className="flex items-center justify-start gap-2 text-sm">
+                  <p>
+                    {new Date(experience.startDate).toLocaleDateString(
+                      "pt-BR",
+                      {
+                        month: "long",
+                        year: "numeric",
+                      },
+                    )}
+                  </p>
                   <Circle
                     className="fill-current text-primary/50 transition ease-in-out hover:text-primary"
                     size={10}
                   />
                   <p>Presente</p>
                 </div>
-                <p className="text-sm font-extralight">{experience.location}</p>
+                <p className="text-sm">{experience.location}</p>
               </div>
 
               {/* PARTE DA DIREITA */}
               <div className="w-full space-y-6 lg:w-[817px]">
                 <h2 className="text-primary">{experience.companyName}</h2>
-                <p className="text-sm font-light">{experience.description}</p>
+                <p className="text-sm">{experience.description}</p>
               </div>
             </div>
           </CardContent>
