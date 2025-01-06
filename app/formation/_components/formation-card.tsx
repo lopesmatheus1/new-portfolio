c505fe54a7df7bc6f2ca3e77e0ec0621eabbc9fd
Matheus Lopes Marendino <91556477+lopesmatheus1@users.noreply.github.com>
2025-01-06 15:56:18 -0300

    feat: add framer-motion animation to formation page

--- app/formation/_components/formation-card.tsx
+++ app/formation/_components/formation-card.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Circle } from "lucide-react";
 import { Formation } from "@/app/_types/page-formation";
 import { Button } from "@/app/_components/ui/button";
@@ -9,6 +10,7 @@ import {
   CardContent,
 } from "@/app/_components/ui/card";
 import { formateDate } from "@/app/_utils/formate-date";
+import { motion } from "motion/react";
 
 type FormationCardProps = {
   formations: Formation[];
@@ -17,39 +19,48 @@ type FormationCardProps = {
 const FormationCard = ({ formations }: FormationCardProps) => {
   return (
     <div className="mt-12 flex h-full flex-col items-center justify-center gap-10 sm:mt-0">
-      {formations.map((formation) => (
-        <Card className="w-full bg-transparent">
-          <CardHeader>
-            <CardTitle className="text-center text-xl font-light sm:text-left">
-              {formation.formationTitle}
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="flex flex-col justify-between gap-5 sm:flex-row">
-              <div className="flex flex-col gap-3 text-sm">
-                <p className="text-center text-sm sm:text-left">
-                  {formation.certificateDescription}
-                </p>
-                <div className="flex items-center justify-center gap-2 text-sm sm:justify-start">
-                  <p>{formateDate(formation.startDate)}</p>
-                  <Circle
-                    className="fill-current text-primary/50 transition ease-in-out hover:text-primary"
-                    size={10}
-                  />
-                  <p>{formateDate(formation.endDate)}</p>
+      {formations.map((formation, i) => (
+        <motion.div
+          key={i + formation.formationTitle}
+          initial={{ opacity: 0, scale: 0 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0 }}
+          transition={{ duration: 0.3, delay: i * 0.4 }}
+          className="w-full"
+        >
+          <Card className="w-full bg-transparent">
+            <CardHeader>
+              <CardTitle className="text-center text-xl font-light sm:text-left">
+                {formation.formationTitle}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col justify-between gap-5 sm:flex-row">
+                <div className="flex flex-col gap-3 text-sm">
+                  <p className="text-center text-sm sm:text-left">
+                    {formation.certificateDescription}
+                  </p>
+                  <div className="flex items-center justify-center gap-2 text-sm sm:justify-start">
+                    <p>{formateDate(formation.startDate)}</p>
+                    <Circle
+                      className="fill-current text-primary/50 transition ease-in-out hover:text-primary"
+                      size={10}
+                    />
+                    <p>{formateDate(formation.endDate)}</p>
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                  <Button variant={"outline"} asChild>
+                    <Link target="_blank" href={formation.pdfCertificate.url}>
+                      Visualizar certificado
+                    </Link>
+                  </Button>
                 </div>
               </div>
-
-              <div className="flex items-center justify-center">
-                <Button variant={"outline"} asChild>
-                  <Link target="_blank" href={formation.pdfCertificate.url}>
-                    Visualizar certificado
-                  </Link>
-                </Button>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+            </CardContent>
+          </Card>
+        </motion.div>
       ))}
     </div>
   );
